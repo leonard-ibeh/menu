a8ed408461ab15abf4c2b2cad98bbb6c4c6b4781
Ibeh Leonard <obinnaibeh42@gmail.com>
2023-09-05 11:07:02 +0100

    Introduced project configuration and helper files.

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -1,16 +1,23 @@
 import { async } from "regenerator-runtime";
+import { TIMEOUT_SEC } from "./config";
+
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
+};
 
 export const getJSON = async function (url) {
   try {
-    const res = await fetch(
-      // "https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886"
-      url
-      // "https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcb6e"
-    );
+    const res = await Promise.race([fetch(url), timeout(TIMEOUT_SEC)]);
     const data = await res.json();
     if (!res.ok) throw new Error(`${data.message} (${res.status})`);
     return data;
   } catch (err) {
-    alert(err);
+    throw err;
   }
 };
+// "https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcb6e"
+// "https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886"
