64da058f876434f4c9b32e3fafdaffe798af0aea
Ibeh Leonard <obinnaibeh42@gmail.com>
2023-09-11 12:31:29 +0100

    Implementing Pagination

--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -1,28 +1,58 @@
 import View from "./View";
-// import icons from "url:../../img/icons.svg";
+import icons from "url:../../img/icons.svg";
 
 class PaginationView extends View {
   _parentElement = document.querySelector(".pagination");
-  _generateMarkup() {
-    const numPages = Math.cell(
+
+  _generateMarkUp() {
+    const curPage = this._data.page;
+    const numPages = Math.ceil(
       this._data.results.length / this._data.resultsPerPage
     );
     console.log(numPages);
 
     // page 1, and there are other pages
-    if (this._data.page === 1 && numPages > 1) {
-      return "page 1, others";
+    if (curPage === 1 && numPages > 1) {
+      return `
+      <button class="btn--inline pagination__btn--next">
+            <span>${curPage + 1}</span>
+            <svg class="search__icon">
+              <use href="${icons}#icon-arrow-right"></use>
+            </svg>
+          </button>
+      `;
     }
     // Last page
-    if (this._data.pages === numPages && numPages > 1) {
-      return "Last page";
+    if (curPage === numPages && numPages > 1) {
+      return `
+      <button class="btn--inline pagination__btn--prev">
+            <svg class="search__icon">
+              <use href="${icons}#icon-arrow-left"></use>
+            </svg>
+            <span>Page ${curPage - 1}</span>
+          </button>
+      `;
     }
     // other page
-    if (this._data.page < numPages) {
-      return "other page";
+    if (curPage < numPages) {
+      return `
+      <button class="btn--inline pagination__btn--prev">
+      <svg class="search__icon">
+        <use href="${icons}#icon-arrow-left"></use>
+      </svg>
+      <span>Page ${curPage - 1}</span>
+    </button>
+
+      <button class="btn--inline pagination__btn--next">
+            <span>${curPage + 1}</span>
+            <svg class="search__icon">
+              <use href="${icons}#icon-arrow-right"></use>
+            </svg>
+          </button>
+      `;
     }
     // page 1, and there are no other pages
-    return "only 1 page";
+    return "";
   }
 }
 
