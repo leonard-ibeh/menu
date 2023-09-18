3fd1c44495363094d9886815caa3524c24d9968b
Ibeh Leonard <obinnaibeh42@gmail.com>
2023-09-18 16:14:31 +0100

    Updating Recipe Servings

--- src/js/controller.js
+++ src/js/controller.js
@@ -70,7 +70,8 @@ const controlServings = function (newServings) {
   model.updateServings(newServings);
 
   // Update the recipe view
-  recipeView.render(model.state.recipe);
+  // recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe);
 };
 
 // window.addEventListener("hashchange", controlRecipe);
--- src/js/views/View.js
+++ src/js/views/View.js
@@ -11,7 +11,18 @@ export default class View {
     this._clear();
     this._parentElement.insertAdjacentHTML("afterbegin", markup);
   }
+  update(data) {
+    if (!data || (Array.isArray(data) && data.length === 0))
+      return this.renderError();
+    this._data = data;
+    const newmarkup = this._generateMarkUp();
 
+    const newDOM = document.createRange().createContextualFragment(newmarkup);
+    const newElements = Array.from(newDOM.querySelectorAll("*"));
+    const curElements = Array.from(this._parentElement.querySelectorAll("*"));
+    console.log(curElements);
+    console.log(newElements);
+  }
   _clear() {
     this._parentElement.innerHTML = "";
   }
