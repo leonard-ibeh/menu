52dbacfb6c2e6eb697c2c56c3117d506d4b1dfd0
Ibeh Leonard <obinnaibeh42@gmail.com>
2023-09-27 19:15:51 +0100

    Refactoring of the codebase

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -3,6 +3,14 @@ import recipeView from "./recipeView";
 
 export default class View {
   _data;
+  /**
+   *  Render the received object to the DOM
+   * @param {object| Object[]} data The data to be rendered (e.g recipe)
+   * @param {Boolean} [render=true] If false, create markup string instead of rendering to the DOM
+   * @returns {undefined | string } A markup string is returned if render=false
+   * @this {object} view instance
+   * @author Leonard Ibeh.
+   */
   render(data, render = true) {
     if (!data || (Array.isArray(data) && data.length === 0))
       return this.renderError();
